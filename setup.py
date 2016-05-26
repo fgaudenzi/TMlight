12749d3f498c412708a59acf87f2ecf8f9a43f47
Filippo Gaudenzi <gaudenzi.filippo@gmail.com>
2016-05-26 10:37:00 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TM light',
+    version='0.1',
+    packages=['testmanager', 'testmanager.model', 'testmanager.tools'],
+    url='https://github.com/fgaudenzi/TMlight',
+    license='GPL',
+    author='filippo gaudenzi',
+    author_email='filippo.gaudenzi@unimi.it',
+    description='Tm light for all in one deployment'
+)
